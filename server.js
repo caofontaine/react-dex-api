6543d6d1f150d811168c730f9785fddf98af1fc7
caofontaine <benjamin.cao88@gmail.com>
2021-06-29 19:22:30 -0400

    add production psql connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(cors());
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: '',
-		password : '',
-		database: 'pokedex'
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
